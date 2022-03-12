2f64bcdd3804bfd1218c27dff057e17de82ddbca
Sxmurai <gavrw0607@gmail.com>
2022-03-12 13:50:40 -0600

    (impl) bruh

--- src/main/java/cope/nebula/client/feature/module/combat/Criticals.java
+++ src/main/java/cope/nebula/client/feature/module/combat/Criticals.java
@@ -6,6 +6,7 @@ import cope.nebula.client.feature.module.Module;
 import cope.nebula.client.feature.module.ModuleCategory;
 import cope.nebula.client.value.Value;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.network.play.client.CPacketPlayer.Position;
 import net.minecraft.network.play.client.CPacketUseEntity;
 import net.minecraft.network.play.client.CPacketUseEntity.Action;
@@ -16,29 +17,26 @@ public class Criticals extends Module {
         super("Criticals", ModuleCategory.COMBAT, "Makes you do critical hits");
     }
 
-    public static final Value<Mode> mode = new Value<>("Mode", Mode.BASIC);
+    public static final Value<Mode> mode = new Value<>("Mode", Mode.STRICT);
 
     @SubscribeEvent
     public void onPacket(PacketEvent event) {
         if (event.getPacket() instanceof CPacketUseEntity && event.getDirection().equals(Direction.OUTGOING)) {
-            CPacketUseEntity packet = (CPacketUseEntity) event.getPacket();
+            CPacketUseEntity packet = event.getPacket();
             if (packet.getAction().equals(Action.ATTACK) && packet.getEntityFromWorld(mc.world) instanceof EntityLivingBase && mc.player.onGround) {
                 switch (mode.getValue()) {
-                    case BASIC: {
+                    case BASIC:
                         mc.player.connection.sendPacket(new Position(mc.player.posX, mc.player.posY + 0.21, mc.player.posZ, false));
                         mc.player.connection.sendPacket(new Position(mc.player.posX, mc.player.posY, mc.player.posZ, false));
                         break;
-                    }
 
-                    case STRICT: {
-                        // TODO
+                    case STRICT:
+                        // TODO still have no idea, its now only flagging for survival fly :/
                         break;
-                    }
 
-                    case JUMP: {
+                    case JUMP:
                         mc.player.jump();
                         break;
-                    }
                 }
             }
         }
