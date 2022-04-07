636aae5065d29677f83fa38b93c1c9993124a360
Sxmurai <gavrw0607@gmail.com>
2022-04-06 20:17:34 -0500

    (impl) Getting ready for Baritone

--- src/main/java/cope/nebula/asm/MixinLoadingPlugin.java
+++ src/main/java/cope/nebula/asm/MixinLoadingPlugin.java
@@ -1,20 +1,29 @@
 package cope.nebula.asm;
 
+import cope.nebula.client.Nebula;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.Name;
 import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.Mixins;
+import org.spongepowered.asm.mixin.transformer.Config;
 
 import javax.annotation.Nullable;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Name("MixinLoadingPlugin")
 @MCVersion("1.12.2")
 public class MixinLoadingPlugin implements IFMLLoadingPlugin {
     public MixinLoadingPlugin() {
         MixinBootstrap.init();
+        MixinEnvironment.getDefaultEnvironment().setObfuscationContext("searge");
+        // Mixins.addConfigurations("mixins.nebula.json", "mixins.baritone.json");
         Mixins.addConfiguration("mixins.nebula.json");
+
+        Nebula.getLogger().info("Loaded mixin configs: " +
+                Mixins.getConfigs().stream().map(Config::getName).collect(Collectors.joining(", ")));
     }
 
     @Override
---
+++ src/main/java/cope/nebula/asm/mixins/client/baritone/MixinBaritoneAPI.java
@@ -0,0 +1,18 @@
+package cope.nebula.asm.mixins.client.baritone;
+
+//import baritone.api.BaritoneAPI;
+//import cope.nebula.client.Nebula;
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.Pseudo;
+//import org.spongepowered.asm.mixin.injection.At;
+//import org.spongepowered.asm.mixin.injection.Inject;
+//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+//
+//@Mixin(BaritoneAPI.class)
+//@Pseudo
+//public class MixinBaritoneAPI {
+//    @Inject(method = "<init>", at = @At("RETURN"))
+//    private static void onInit(CallbackInfo info) {
+//        Nebula.getLogger().info("Initialized baritone API.");
+//    }
+//}
