fa856946f65059b6f7e3b63ee9ce090b8d41f9b4
Sxmurai <gavrw0607@gmail.com>
2022-04-06 20:56:23 -0500

    (impl) Still broken strafe

--- src/main/java/cope/nebula/client/feature/module/movement/Speed.java
+++ src/main/java/cope/nebula/client/feature/module/movement/Speed.java
@@ -2,12 +2,10 @@ package cope.nebula.client.feature.module.movement;
 
 import cope.nebula.client.events.MotionEvent;
 import cope.nebula.client.events.MotionUpdateEvent;
-import cope.nebula.client.events.MotionUpdateEvent.Era;
 import cope.nebula.client.events.PacketEvent;
 import cope.nebula.client.events.PacketEvent.Direction;
 import cope.nebula.client.feature.module.Module;
 import cope.nebula.client.feature.module.ModuleCategory;
-import cope.nebula.client.feature.module.render.hud.impl.Info;
 import cope.nebula.client.value.Value;
 import cope.nebula.util.internal.math.Vec2d;
 import cope.nebula.util.renderer.FontUtil;
@@ -17,8 +15,16 @@ import net.minecraft.network.play.server.SPacketPlayerPosLook;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import static cope.nebula.client.feature.module.world.Timer.setTimerSpeed;
 
+/**
+ * Credits: https://github.com/Sxmurai/not-konas-src/blob/main/src/main/java/me/darki/konas/module/modules/movement/Speed.java
+ *
+ * I again, cannot make movement hacks for shit
+ */
 public class Speed extends Module {
     public Speed() {
         super("Speed", ModuleCategory.MOVEMENT, "Makes you go faster");
@@ -51,18 +57,15 @@ public class Speed extends Module {
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void onMotionUpdate(MotionUpdateEvent event) {
-        if (event.getEra().equals(Era.PRE)) {
-            distance = Math.sqrt(Math.pow(mc.player.posX - mc.player.prevPosX, 2) + Math.pow(mc.player.posZ - mc.player.prevPosZ, 2));
-        }
+        distance = Math.sqrt(Math.pow(mc.player.posX - mc.player.prevPosX, 2) + Math.pow(mc.player.posZ - mc.player.prevPosZ, 2));
     }
 
     @SubscribeEvent
     public void onPacket(PacketEvent event) {
         if (event.getDirection().equals(Direction.INCOMING) && event.getPacket() instanceof SPacketPlayerPosLook) {
             strafeStage = 4;
+            moveSpeed = MotionUtil.getBaseNCPSpeed();
             distance = 0;
-
-            sendChatMessage("MoveSpeed: " + moveSpeed + " (" + Info.getSpeedInKmh() + "kmh)" + ", OddStage: " + slowdown);
         }
     }
 
@@ -82,14 +85,27 @@ public class Speed extends Module {
                     } else {
                         setTimerSpeed(1.15f);
 
-                        event.setX(event.getX() * 1.19);
-                        event.setZ(event.getZ() * 1.19);
+                        event.setX(event.getX() * 1.019);
+                        event.setZ(event.getZ() * 1.019);
+                    }
+                }
+
+                if (mode.getValue().equals(Mode.STRICT_STRAFE)) {
+                    if (round(mc.player.posY - (int) mc.player.posY, 3) == round(0.138, 3)) {
+                        mc.player.motionY -= 0.08;
+                        event.setY(event.getY() - 0.09316090325960147);
+                        mc.player.motionY -= 0.09316090325960147;
                     }
                 }
             }
 
             if (strafeStage == 1) {
-                moveSpeed = 1.35 * getBaseNCPSpeed() - 0.01;
+                double multi = 1.35;
+                if (mode.getValue().equals(Mode.STRICT_STRAFE)) {
+                    multi = 1.38;
+                }
+
+                moveSpeed = multi * getBaseNCPSpeed() - 0.01;
             } else if (strafeStage == 2) {
                 if (MotionUtil.isMoving() && mc.player.onGround) {
                     double motionY = getJumpHeight(mode.getValue().equals(Mode.STRICT_STRAFE));
@@ -97,25 +113,34 @@ public class Speed extends Module {
                     mc.player.motionY = motionY;
                     event.setY(motionY);
 
-                    double acceleration = 2.149;
-                    if (mc.player.ticksExisted % 2 == 0) {
-                        acceleration = 1.9828;
+                    if (mode.getValue().equals(Mode.STRAFE)) {
+                        if (slowdown) {
+                            moveSpeed *= 1.395;
+                        } else {
+                            moveSpeed *= 1.7835;
+                        }
+                    } else {
+                        moveSpeed *= 2.149;
                     }
-
-                    moveSpeed *= acceleration;
                 }
             } else if (strafeStage == 3) {
-                double diff = 0.76 * (distance - getBaseNCPSpeed());
+                double diff = 0.66 * (distance - getBaseNCPSpeed());
                 moveSpeed = distance - diff;
+                slowdown = !slowdown;
             } else {
                 if (!mc.world.getCollisionBoxes(mc.player, mc.player.getEntityBoundingBox().offset(0.0, mc.player.motionY, 0.0)).isEmpty()) {
-                    strafeStage = MotionUtil.isMoving() ? 3 : 1;
+                    strafeStage = 1;
                 }
 
                 moveSpeed = distance - distance / 159.0;
             }
 
             moveSpeed = Math.max(moveSpeed, getBaseNCPSpeed());
+
+            // limit out strict strafe speed
+            if (mode.getValue().equals(Mode.STRICT_STRAFE)) {
+                moveSpeed = Math.min(moveSpeed, mc.player.ticksExisted % 6 == 0 ? 0.524 : 0.511);
+            }
 
             Vec2d motion = MotionUtil.strafe(moveSpeed);
 
@@ -145,7 +170,7 @@ public class Speed extends Module {
      * @return the vanilla jump height
      */
     private double getJumpHeight(boolean strict) {
-        double y = strict ? 0.42 : 0.3995;
+        double y = strict ? 0.3995 : 0.3999999463558197;
         if (mc.player.isPotionActive(MobEffects.JUMP_BOOST)) {
             y += (mc.player.getActivePotionEffect(MobEffects.JUMP_BOOST).getAmplifier() + 1) * 0.1;
         }
@@ -153,6 +178,11 @@ public class Speed extends Module {
         return y;
     }
 
+    private double round(double value, int places) {
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
 
     public enum Mode {
         STRAFE, STRICT_STRAFE
