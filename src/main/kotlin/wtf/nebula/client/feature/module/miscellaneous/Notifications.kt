29348b2690419a53b9c78435c08c23207f8ee2bf
Sxmurai <gavrw0607@gmail.com>
2022-07-10 22:13:53 -0500

    impl: notifications shit

---
+++ src/main/kotlin/wtf/nebula/client/feature/module/miscellaneous/Notifications.kt
@@ -0,0 +1,7 @@
+package wtf.nebula.client.feature.module.miscellaneous
+
+import wtf.nebula.client.feature.module.Module
+import wtf.nebula.client.feature.module.ModuleCategory
+
+class Notifications : Module(ModuleCategory.MISCELLANEOUS, "Notifications and shit") {
+}
